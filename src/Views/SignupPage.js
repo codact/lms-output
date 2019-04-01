c1c3143190431838a9c86d5d2c4cbc65c05f65b5
codact <savalijey@gmail.com>
2019-04-01 11:35:08 +0300

    Add signup and login page

--- src/Views/SignupPage.js
+++ src/Views/SignupPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Signup } from '../components/Signup/Signup.jsx'
 
 class SignupPage extends Component {
+
     render() {
         return (
             <div>
--- src/components/Home/HomeDisplay.jsx
+++ src/components/Home/HomeDisplay.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 export const HomeDisplay = () => {
     return (
         <div className="home-content">
+            <h5>&copy;James Savali</h5>
             <h1>Store Manager</h1>
             <Link className="btn" to={'/login'} role="button">
                 Login
