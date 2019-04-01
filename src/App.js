ba26a4f3b4cf115d2e18a44109353c6d974151fd
codact <savalijey@gmail.com>
2019-04-01 11:33:12 +0300

    Add signup and login page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/Navbar.jsx';
-import Home from './Views/Home.jsx';
-import LoginPage from './Views/LoginPage.jsx';
-import SignupPage from './Views/SignupPage.jsx'
+import Home from './Views/Home.js';
+import LoginPage from './Views/LoginPage.js';
+import SignupPage from './Views/SignupPage.js'
+import './index.css'
 
 export default class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <Navbar />
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/login" component={LoginPage} />
